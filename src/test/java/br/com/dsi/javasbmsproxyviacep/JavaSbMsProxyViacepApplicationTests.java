9bccfa2bbb0dc60864b401c4177269a3033cdc6d
Giovanni Donda <grdonda@gmail.com>
2024-10-03 14:58:05 -0300

    start

---
+++ src/test/java/br/com/dsi/javasbmsproxyviacep/JavaSbMsProxyViacepApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dsi.javasbmsproxyviacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSbMsProxyViacepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
